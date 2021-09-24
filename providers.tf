f3e91c1a571cc7a39da85d09d8d2c290386932cb
eduhkbr <edu.abreu09@hotmail.com>
2021-09-23 21:47:35 -0300

    Convertendo Credential em Variável

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto provider "google"
 provider "google" {
 
-	credentials = file("/home/eduardo/Downloads/erp-test-202020-fb7a70a703a8.json")
-
 	project = "erp-test-202020"
 	region = "us-central1"
 	zone = "us-central1-c"
