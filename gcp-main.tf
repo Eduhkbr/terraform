ece661949b23e7538b304da71b195ed019aaeac7
eduhkbr <edu.abreu09@hotmail.com>
2021-09-23 22:00:47 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -21,3 +21,8 @@ resource "google_compute_instance" "firstvm" {
 	  #a presença do bloco access_config garante que a instância estará acessivel pela internet mesmo que vazia
 	}
 }
+
+resource "google_app_engine_application" "app"
+	project = "erp-test-202020"
+	location_id = "us-central"
+}
